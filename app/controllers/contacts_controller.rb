2eab052b6bd3bea1947776dbe82fa7b862281969
weillium <will.liao@kellogg.northwestern.edu>
2022-02-15 17:31:45 +0000

    exercise and lab

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,4 +4,15 @@ class ContactsController < ApplicationController
     @contact = Contact.find(params["id"])
   end
 
+  def new
+    @contact = Contact.new
+    @contact.company_id = params["company_id"]
+  end
+
+  def create
+    @contact = Contact.new(params["contact"])
+    @contact.save
+    redirect_to "/companies"
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,14 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params["post"])
+    @post.save
+    redirect_to "/posts"
+  end
+
 end
